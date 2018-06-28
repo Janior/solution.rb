a84965c2de0a66a061f4a0b5dbc3069f5a8b24ac
Janior <janior_orlando@hotmail.com>
2018-06-28 14:07:04 -0500

    first commit

---
+++ solution.rb
@@ -0,0 +1,15 @@
+#https://github.com/Janior/solution.rb.git
+
+require "sinatra"
+
+get '/' do
+  unless params[:nombre]
+     "Hola desconocido!"
+  else
+     "Hola #{params[:nombre]}"
+  end
+end
+
+get '/:nombre' do
+  "Hola #{params[:nombre]}"
+end
