bc6f4b29cfea38b4dd008b10df51099e8209bb5e
Janior <janior_orlando@hotmail.com>
2018-07-04 13:55:17 -0500

    Update solution.rb
    Saluame 3
    Cuando el usuario le da click al botón "¡Hola!", la aplicación le debe mostrar otra página que diga "¡Hola !" (reemplaza por el nombre de la persona).
    Nota:* El nombre debe quedar tal cual como esta escrito en el input, no lo capitalizes ni le hagas modificaciones.

--- solution.rb
+++ solution.rb
@@ -1,10 +1,27 @@
 #https://github.com/Janior/solution.rb
-
 require "sinatra"
+
 get '/' do	
-	erb :index
+	unless params[:nombre]
+		"hola "
+	else
+		"Hola #{params[:nombre]}"
+	end
+
+	<<-HTML
+    <form action="/nuevo/objeto" method="post">
+    	<h1>Hola dime a quien saludar</h1>
+	   <input type="text" name="verbo"></input>
+       <input type="submit" value="submit"></input>
+    </form>
+	HTML
 end
 
-post '/saludando' do 
-	"Hola #{params[:txtNombre].capitalize!}"
+
+get '/:nombre' do
+  "Hola #{params[:nombre]}"
+end
+
+post '/nuevo/objeto' do
+	"¡Hola !#{params[:verbo]}"
 end
