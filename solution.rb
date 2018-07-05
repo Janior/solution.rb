4cde4ee6be78fd6f012c8145126aeb10bc2ffd28
Janior <janior_orlando@hotmail.com>
2018-07-05 13:27:56 -0500

    Update solution.rb
    Crea una carpeta con un archivo llamado solution.rb.
    Dentro del archivo solution.rb crea la aplicación de Sinatra con una única ruta GET / que revele el código secreto.
    Inicia el servidor ejecutando el comando ruby solution.rb, e ingresa desde un navegador a http://localhost:4567/. Deberás ver el código secreto.
    Publica tu solución en Github. 
    Ingresa el repositorio con tu solución.

--- solution.rb
+++ solution.rb
@@ -1,27 +1,6 @@
 #https://github.com/Janior/solution.rb
 require "sinatra"
 
-get '/' do	
-	unless params[:nombre]
-		"hola "
-	else
-		"Hola #{params[:nombre]}"
-	end
-
-	<<-HTML
-    <form action="/nuevo/objeto" method="post">
-    	<h1>Hola dime a quien saludar</h1>
-	   <input type="text" name="verbo"></input>
-       <input type="submit" value="submit"></input>
-    </form>
-	HTML
-end
-
-
-get '/:nombre' do
-  "Hola #{params[:nombre]}"
-end
-
-post '/nuevo/objeto' do
-	"¡Hola !#{params[:verbo]}"
+get '/' do 
+  "HcUy6Re2LLBRtj"
 end
