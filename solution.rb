897d93be7bd080b9a28e0de925c99a77a2fdd101
Janior <janior_orlando@hotmail.com>
2018-07-05 19:02:10 -0500

    Update solution.rb
    sin tildes

--- solution.rb
+++ solution.rb
@@ -2,23 +2,17 @@
 
 require "sinatra"
 
-if RUBY_VERSION =~ /1.9/
-    Encoding.default_external = Encoding::UTF_8
-    Encoding.default_internal = Encoding::UTF_8
-end
-	
 get '/' do
 	erb :index
 end
-
 
 post '/diceabuelita' do
 	@elmensaje= params[:txtMensaje]
 	if !@elmensaje.empty? 
 		if @elmensaje == @elmensaje.upcase
-			@elmensaje = "Ahhh si, manzanas!"
+			@elmensaje = "Ahhh si, manzanas"
 		else
-			@elmensaje = "Habla más duro mijito"
+			@elmensaje = "Habla mas duro mijito"
 		end
 		erb :diceabuelita
 	else
