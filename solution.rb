a35556242143fde39e967eadb571694553c7fa24
Janior <janior_orlando@hotmail.com>
2018-07-05 19:01:11 -0500

    Update solution.rb
    if RUBY_VERSION =~ /1.9/
        Encoding.default_external = Encoding::UTF_8
        Encoding.default_internal = Encoding::UTF_8
    end

--- solution.rb
+++ solution.rb
@@ -1,7 +1,11 @@
 #https://github.com/Janior/solution.rb
 
 require "sinatra"
-<meta charset="UTF-8">
+
+if RUBY_VERSION =~ /1.9/
+    Encoding.default_external = Encoding::UTF_8
+    Encoding.default_internal = Encoding::UTF_8
+end
 	
 get '/' do
 	erb :index
