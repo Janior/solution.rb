9afd90ea8c427cdcc516001100c1f783383d9aad
Janior <janior_orlando@hotmail.com>
2018-07-06 20:34:53 -0500

    Update solution.rb
    Sin Permiso! 
    180 puntos 
    Hola Maker!, 
    Estamos en el proceso de crear un sistema de autenticación, y necesitamos tu ayuda para terminar de implementarlo. 
    El sistema funciona de la siguiente manera: 
    Un usuario realiza un request a nuestro servidor, dentro del cual envía el header personalizado permiso con el valor soy-un-token-secreto. 
    El servidor recibe el request y revisa si contiene el header permiso con el valor esperado. 
    En caso de que la condición anterior se cumpla el servidor le permite al cliente entrar a nuestra zona privada. En caso contrario le niega el acceso. 
    Hasta este momento ya tenemos listo el comando mediante el cual el usuario podrá hacer el request autenticado, el cual es el siguiente:

--- solution.rb
+++ solution.rb
@@ -1,8 +1,14 @@
-#https://github.com/Janior/solution.rb
+# https://github.com/Janior/solution.rb
+# esto es lo que se debe ingresar en el explorador --> curl -XGET -H "permiso: soy-un-token-secreto" "http://localhost:4567/"
+# es necesario descargar curl
 
 require "sinatra"
 
 get '/' do
-	@browser = request.user_agent
-	erb :index
+  token = request.env["HTTP_PERMISO"]
+  @responder = "Sin Permiso"
+  if token == "soy-un-token-secreto"
+    @responder = "Si lo logramos!"
+  end
+  erb :index
 end
